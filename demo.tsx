a388c63e07378b05467030d673c7b93f0a957347
Readmecodegen <onlysonuk2@gmail.com>
2026-06-14 15:50:12 +0530

    Update demo.tsx

--- demo.tsx
+++ demo.tsx
@@ -1,5 +1,5 @@
 this is demo file created to check is the web hook works or not if new file created.
-  ok now its 22 minutes over and currently my dynamic file tree is not updated with the newly added demo.tsx.
-  if u see the added this new file in the readme then all is correct but if not then please raise the issue.
+  ok  refresh the browser now its works ..
+  if not yours then please raise the issue.
   And contribute to solve this problem.
 thanku..
